922f16abed1fb700ca45b6127ea2c7612bd137c4
ksknight <korvette_00Whotmail.com>
2019-03-15 13:55:51 -0600

    created country array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,67 +3,67 @@ var countries = [{
     city: "Bruges",
     image: "",
     code: "BA",
-}{
+},{
     name: "Brazil",
     city: "Rio de Janeiro",
     image: "",
     code: "BR",
-}{
+},{
     name: "Canada",
     city: "Vancouver",
     image: "",
     code: "CA",
-}{
+},{
     name: "Czech Republic",
     city: "Prague",
     image: "",
     code: "CZ",
-}{
+},{
     name: "Denmark",
     city: "Helsingør",
     image: "",
     code: "DK",
-}{
+},{
     name: "Germany",
     city: "Berlin",
     image: "",
     code: "DE",
-}{
+},{
     name: "France",
     city: "Bordeaux ",
     image: "",
     code: "FR",
-}{
+},{
     name: "Norway",
     city: "Oslo",
     image: "",
     code: "NO",
-}{
+},{
     name: "Poland",
     city: "Kraków",
     image: "",
     code: "PL",
-}{
+},{
     name: "Russia",
     city: "Novosibirsk",
     image: "",
     code: "RU",
-}{
+},{
     name: "Slovakia",
     city: "High Tatras",
     image: "",
     code: "SK",
-}{
+},{
     name: "Sierra Leone",
     city: "Freetown",
     image: "",
     code: "SL",
-}{
+},{
     name: "Vietnam",
     city: "Hoi An",
     image: "",
     code: "VN",
-}{
+},{
     name: "Indonesia",
     city: "Bali",
     image: "",
