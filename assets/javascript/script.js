819a6aeb4bcf7f9b17e83353e59ecd67092bcf34
ksknight <korvette_00Whotmail.com>
2019-03-19 12:34:29 -0600

    reorganised and merged all user data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,11 +6,7 @@ var countries = [{
     city: "Bruges",
     image: ["assets/images/bruges1.jpg", "assets/images/bruges2.jpg", "assets/images/bruges3.jpg", "assets/images/bruges4.jpg"],
     code: "BA",
-},{
-
-    code: "Be",
-    latitude: "51.2093",
-},{
+}, {
 
     name: "Brazil",
     city: "Rio de Janeiro",
@@ -59,41 +55,8 @@ var countries = [{
     image: ["assets/images/kraków1.jpg", "assets/images/kraków2.jpg", "assets/images/kraków3.jpg", "assets/images/kraków4.jpg"],
     code: "PL",
 
-}, {
-    name: "Russia",
-    city: "Moscow",
-    image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
-    code: "RU",
+},
 
-}, {
-    name: "Slovakia",
-    city: "High Tatras",
-    image: ["assets/images/high-tatras1.jpg", "assets/images/high-tatras2.jpg", "assets/images/high-tatras3.jpg", "assets/images/high-tatras4.jpg"],
-    code: "SK",
-
-}, {
-    name: "Sierra Leone",
-    city: "Freetown",
-    image: ["assets/images/freetown1.jpg", "assets/images/freetown2.jpg", "assets/images/freetown3.jpg", "assets/images/freetown4.jpg"],
-    code: "SL",
-
-}, {
-    name: "Vietnam",
-    city: "Hoi An",
-    image: ["assets/images/hoi-an1.jpg", "assets/images/hoi-an2.jpg", "assets/images/hoi-an3.jpg", "assets/images/hoi-an4.jpg"],
-    code: "VN",
-
-}, {
-    name: "Indonesia",
-    city: "Bali",
-    image: ["assets/images/bali1.jpg", "assets/images/bali2.jpg", "assets/images/bali3.jpg", "assets/images/bali4.jpg"],
-    code: "ID",
-
-}
-];
-
-
-}, 
 // NEWLY ADDED CITIES
 {
     name: "Argentina",
@@ -102,40 +65,38 @@ var countries = [{
 
     code: "NONE",
 
-},{
+}, {
     name: "Australia",
     city: "Sydney",
     image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
     code: "NONE",
 
-},{
+}, {
     name: "Austria",
     city: "Wien",
     image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
     code: "NONE",
 
-},{
+}, {
     name: "Chile",
     city: "Santiago",
     image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
     code: "NONE",
 
-},{
+}, {
     name: "Philippines",
     city: "Manila",
     image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
     code: "NONE",
 
-},{
+}, {
     name: "Singapore",
     city: "Singapore",
     image: ["assets/images/novosibirsk1.jpg", "assets/images/novosibirsk2.jpg", "assets/images/novosibirsk3.jpg", "assets/images/novosibirsk4.jpg"],
     code: "NONE",
 
 },
-
-
-
+];
 
 
 
@@ -145,32 +106,11 @@ $(document).ready(function () {
     $(".dropdown-trigger").dropdown();
 });
 
-$(".find-city").on("click", function(event) {
-var cityNumber = (Math.floor(Math.random()* countries.length));
-$(".city-name").text(countries[cityNumber].city)
-$(".country-name").text(countries[cityNumber].name)
-
-
-$(".find-city").on("click", function(event) {
-var cityNumber = (Math.floor(Math.random()* countries.length));
-$(".city-name").text(countries[cityNumber].city)
-$(".country-name").text(countries[cityNumber].name)
-
 $(".find-city").on("click", function (event) {
-
     var cityNumber = (Math.floor(Math.random() * countries.length));
+    // sets city name on page
     $(".city-name").text(countries[cityNumber].city)
     $(".country-name").text(countries[cityNumber].name)
-
-
-
-
-var queryURLEventsToken = "https://www.eventbriteapi.com/v3/users/me/?token=XMB4Y3P46DMGD4HK5LHA";
-var queryURLEvents = "https://www.eventbriteapi.com/v3/events/search/?token=XMB4Y3P46DMGD4HK5LHA&location.address=" + countries[cityNumber].city + "&location.within=10km&expand=venue";
-
-
-
-$(document).ready(function() {
 
     $("#image1").attr("src", countries[cityNumber].image[0]);
     $("#image2").attr("src", countries[cityNumber].image[1]);
@@ -178,213 +118,195 @@ $(document).ready(function() {
     $("#image4").attr("src", countries[cityNumber].image[3]);
 
 
+    // AJAX CALL FOR EVENTS
+    var queryURLEventsToken = "https://www.eventbriteapi.com/v3/users/me/?token=XMB4Y3P46DMGD4HK5LHA";
+    var queryURLEvents = "https://www.eventbriteapi.com/v3/events/search/?token=XMB4Y3P46DMGD4HK5LHA&location.address=" + countries[cityNumber].city + "&location.within=10km&expand=venue";
+
     $.ajax({
         url: queryURLEventsToken,
         method: "GET"
-    }).then (function(response){
+    }).then(function (response) {
         $.ajax({
             url: queryURLEvents,
             method: "GET"
-        }).then (function(response){
-
-
-            $("tbody").empty();
-            $("thead").empty();
-
-            console.log(response)
-
+        }).then(function (response) {
             $("#events-body").empty();
             $("#events-head").empty();
 
-           
-
-        if (response.events.length >= 1) {
-            var newHeader = $("<tr>").append(
-                $("<th>").text("Link:").css("font-weight","Bold"),
-                $("<th>").text("Event Name:").css("font-weight","Bold"),
-                $("<th>").text("Snapshot:").css("font-weight","Bold"),
-                $("<th>").text("Date:").css("font-weight","Bold")
-            )
-            $("#events-head").append(newHeader);
-
-            for (var i=0; i<5; i++) {
-                var eventName = response.events[i].name.text
-                var eventSummary = response.events[i].summary
-                var eventDate = response.events[i].start.local
-                var eventURL = response.events[i].url 
-                var eventLink = eventURL.link(eventURL);
-
-               
-                var date = eventDate.split("T");
-                var momentDate = moment(date[0], "YYYY-MM-DD");
-                var finalDate = momentDate.format("MMMM Do YYYY")
-
-                $("a").attr('target', '_blank')
-
-                var newRow = $("<tr>").append(
-                    $("<td>").html(eventLink),
-                    $("<td>").text(eventName),
-                    $("<td>").text(eventSummary),
-                    $("<td>").text(finalDate),     
+            if (response.events.length >= 1) {
+                var newHeader = $("<tr>").append(
+                    $("<th>").text("Link:").css("font-weight", "Bold"),
+                    $("<th>").text("Event Name:").css("font-weight", "Bold"),
+                    $("<th>").text("Snapshot:").css("font-weight", "Bold"),
+                    $("<th>").text("Date:").css("font-weight", "Bold")
                 )
-                
-                $("#events-body").append(newRow);
+                $("#events-head").append(newHeader);
+
+                for (var i = 0; i < 5; i++) {
+                    var eventName = response.events[i].name.text
+                    var eventSummary = response.events[i].summary
+                    var eventDate = response.events[i].start.local
+                    var eventURL = response.events[i].url
+                    var eventLink = eventURL.link(eventURL);
+
+
+                    var date = eventDate.split("T");
+                    var momentDate = moment(date[0], "YYYY-MM-DD");
+                    var finalDate = momentDate.format("MMMM Do YYYY")
+
+                    $("a").attr('target', '_blank')
+
+                    var newRow = $("<tr>").append(
+                        $("<td>").html(eventLink),
+                        $("<td>").text(eventName),
+                        $("<td>").text(eventSummary),
+                        $("<td>").text(finalDate),
+                    )
+
+                    $("#events-body").append(newRow);
+                }
+            } else {
+                var none = $()
+                $("#events-head").append($("<h5>Sorry, no event data available for this city.</h5>"));
             }
-        } else {
-            var none = $()
-            $("#events-head").append($("<h5>Sorry, no event data available for this city.</h5>"));
-        }
         })
 
-        });
-
-}) 
-
-// })
-
-
-
-
+    });
+    // END AJAX CALL FOR EVENTS
 
     // AJAX CALL FOR HOLIDAYS
-    if (countries[cityNumber].code === "NONE"){
+    if (countries[cityNumber].code === "NONE") {
         $("#data").empty();
         $("#data").append("My apologies. Holiday information is unavailable");
 
     }
     else {
-    var year = moment().format("YYYY");
-    var month = moment().format("MM");
-    var day = moment().format("DD");
-    var queryURLholiday = ("https://holidayapi.pl/v1/holidays?country=" + countries[cityNumber].code + "&year=" + year + "&month=" + month + "&day=" + day + "&upcoming=true");
+        var year = moment().format("YYYY");
+        var month = moment().format("MM");
+        var day = moment().format("DD");
+        var queryURLholiday = ("https://holidayapi.pl/v1/holidays?country=" + countries[cityNumber].code + "&year=" + year + "&month=" + month + "&day=" + day + "&upcoming=true");
 
-    $.ajax({
-        url: queryURLholiday,
-        method: "GET"
+        $.ajax({
+            url: queryURLholiday,
+            method: "GET"
 
-    }).then(function (response) {
-        $("#data").empty();
-        for (var i = 0; i < response.holidays.length; i++) {
-            var holidayName = $("<div>");
-            var holidayDate = $("<div>");
-            
-            var uglyDate = response.holidays[i].date
-            var convertingDate = moment(uglyDate, "YYYY-MM-DD");
-            var prettyDate = (convertingDate).format("MMMM Do YYYY");
-            console.log(prettyDate);
+        }).then(function (response) {
+            $("#data").empty();
+            for (var i = 0; i < response.holidays.length; i++) {
+                var holidayName = $("<div>");
+                var holidayDate = $("<div>");
 
-            holidayName.append(response.holidays[i].name);
-            holidayDate.append(prettyDate);
-            $("#data").append(holidayName, holidayDate);
-        }
-    })};
+                var uglyDate = response.holidays[i].date
+                var convertingDate = moment(uglyDate, "YYYY-MM-DD");
+                var prettyDate = (convertingDate).format("MMMM Do YYYY");
+                console.log(prettyDate);
 
-
-    // AJAX CALL FOR Weather
-    //api key for weather    
-    var APIKeyWeather = "166a433c57516f51dfab1f7edaed8413"
-    var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + countries[cityNumber].city + "," + countries[cityNumber].name + "&units=imperial&cnt=7&appid=" + APIKeyWeather;
-    //api key for flight
+                holidayName.append(response.holidays[i].name);
+                holidayDate.append(prettyDate);
+                $("#data").append(holidayName, holidayDate);
+            }
+        })
+    };
+    // END AJAX CALL FOR HOLIDAYS
 
 
     // AJAX CALL FOR YELP
     var APIKeyYelp = "vM6YWm9IAxDZYTbuxk8D_w1rBB0rxOtmRZW_xkTwsmSM93dTTRHRdXShK9PM8TW64q-cxa-YpYSM47o-b5U-rtQoNMrdxHm--JFfFakqzqIAlZDtwmtxl7hASvCPXHYx";
     var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
     var queryURLYelp = "https://api.yelp.com/v3/businesses/search?term=restaurant&radius=40000&location=" + countries[cityNumber].city + "%2C%20" + countries[cityNumber].name;
-
-    $.ajax({
-        url: queryURLYelp,
-        method: "GET",
-        Authorization: "Bearer vM6YWm9IAxDZYTbuxk8D_w1rBB0rxOtmRZW_xkTwsmSM93dTTRHRdXShK9PM8TW64q-cxa-YpYSM47o-b5U-rtQoNMrdxHm--JFfFakqzqIAlZDtwmtxl7hASvCPXHYx",
-        cache: true,
-        crossDomain: true,
-        dataType: "jsonp",
-        contentType: "application/json"
-    }).then(function (response) {
-        console.log(response)
-    })
-    console.log(queryURLYelp);
-
-    
     var settings = {
         "async": true,
         "crossDomain": true,
         "url": corsAnywhere + queryURLYelp,
         "method": "GET",
         "headers": {
-          "Authorization": "Bearer vM6YWm9IAxDZYTbuxk8D_w1rBB0rxOtmRZW_xkTwsmSM93dTTRHRdXShK9PM8TW64q-cxa-YpYSM47o-b5U-rtQoNMrdxHm--JFfFakqzqIAlZDtwmtxl7hASvCPXHYx",
+            "Authorization": "Bearer vM6YWm9IAxDZYTbuxk8D_w1rBB0rxOtmRZW_xkTwsmSM93dTTRHRdXShK9PM8TW64q-cxa-YpYSM47o-b5U-rtQoNMrdxHm--JFfFakqzqIAlZDtwmtxl7hASvCPXHYx",
 
         }
-      }
-      
-      $.ajax(settings).done(function (response) {
+    }
+
+    $.ajax(settings).done(function (response) {
         console.log(response);
-      });
+    });
+    // END AJAX CALL FOR YELP
 
 
-    // sets city name on page
-    $(".city-name").text(countries[cityNumber].city);
-    $(".country-name").text(countries[cityNumber].name);
 
-        //weather call
+
+    // AJAX CALL FOR Weather
+
+    //api key for weather    
+    var APIKeyWeather = "166a433c57516f51dfab1f7edaed8413"
+    var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + countries[cityNumber].city + "," + countries[cityNumber].name + "&units=imperial&cnt=7&appid=" + APIKeyWeather;
+
+
+    //weather call
         $.ajax({
             url: queryURLWeather,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response)
-            for (var i=0; i<5; i++) {
-                $("#weather-text").empty("");
+            $("#weather-head").empty("");
+            $("#weather-text").empty("");
+            $("#weather-text").append("<h3>5 day forecast</h3>");
+
+            for (var i = 0; i < 5; i++) {
                 $("#weather-text").attr("class", "teal-text")
-                $("#weather-text").append("<h3>5 day forecast</h3>");
-                $("#weather-head").empty("");
                 var weatherHeader = $("<tr>").append(
-                    $("<th>").text("Date").css("font-weight","Bold"),
-                    $("<th>").text("Temperature (F)").css("font-weight","Bold"),
-                    $("<th>").text("Humidity %").css("font-weight","Bold"),
-                    $("<th>").text("Sky coverage").css("font-weight","Bold"),
+                    $("<th>").text("Date").css("font-weight", "Bold"),
+                    $("<th>").text("Temperature (F)").css("font-weight", "Bold"),
+                    $("<th>").text("Humidity %").css("font-weight", "Bold"),
+                    $("<th>").text("Sky coverage").css("font-weight", "Bold"),
                     $("<th>").text("Windspeed (MPH)").css("font-weight", "Bold")
                 );
                 $("#weather-head").append(weatherHeader);
                 var weatherDay = response.list[i].dt_txt;
                 var weatherTemp = response.list[i].main.temp;
                 var weatherHumidity = response.list[i].main.humidity;
-                var weatherCloud = response.list[i].weather[0].description; 
+                var weatherCloud = response.list[i].weather[0].description;
                 var weatherSpeed = response.list[i].wind.speed;
-
-
-
                 var newRow = $("<tr>").append(
-                    $("<td>").text(weatherDay), 
+                    $("<td>").text(weatherDay),
                     $("<td>").html(weatherTemp),
                     $("<td>").text(weatherHumidity),
                     $("<td>").text(weatherCloud),
-                    $("<td>").text(weatherSpeed),  
-                      
+                    $("<td>").text(weatherSpeed),
+
                 )
-                
+
                 $("#weather-body").append(newRow);
             }
-            $(".current-weather").html("Temperature (F): " + response.main.temp + "</br>Humidity: " + response.main.humidity + "</br>Sky coverage: " + response.weather[0].description + "</br>Wind speed (MPH): " + response.wind.speed)
             // incase weather datadoesnt exist
-        }).fail(function() {
+        }).fail(function () {
             $("#weather-text").html("No weather data exists for " + countries[cityNumber].city + ", " + countries[cityNumber].name)
 
         });
+ 
+
+    // END AJAX CALL FOR Weather
 
 
-    // END OF ON CLICK FUNCTION FOR FIND CITY
-    })
+
+    // AJAX CALL FOR CURRENCY
 
     // var APIKeyCurrency = "eaa992c0be1e3c86d2a8"
     // var queryURLCurrency = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_PHP&compact=ultra&apiKey=eaa992c0be1e3c86d2a8"
-        
+
     // $.ajax({
     //     url: queryURLCurrency,
     //     method: "GET"
     // }).then(function(response) {
     //     console.log(response)
 
-        
+
     // })    
+
+    // END AJAX CALL FOR CURRENCY
+
+
+
+
+
+    // END OF ON CLICK FUNCTION FOR FIND CITY
 })
 
